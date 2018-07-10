a4a77f700b0a5e25281cc2b33db15ecdd8d6de26
gyermich <yermichevagalina@gmail.com>
2018-07-10 23:03:38 +0000

    passing data - first commit

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+/*
+Display a list of movies where each movie contains a list of users that favorited it.
+
+For detailed instructions, refer to instructions.md.
+*/
+
+const profiles = [
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 2,
+    userID: '2',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 3,
+    userID: '4',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 4,
+    userID: '5',
+    favoriteMovieID: '2',
+  },
+  {
+    id: 5,
+    userID: '3',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 6,
+    userID: '6',
+    favoriteMovieID: '4',
+  },
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: 'Jane Jones',
+    userName: 'coder',
+  },
+  2: {
+    id: 2,
+    name: 'Matthew Johnson',
+    userName: 'mpage',
+  },
+  3: {
+    id: 3,
+    name: 'Autumn Green',
+    userName: 'user123',
+  },
+  4: {
+    id: 3,
+    name: 'John Doe',
+    userName: 'user123',
+  },
+  5: {
+    id: 5,
+    name: 'Lauren Carlson',
+    userName: 'user123',
+  },
+  6: {
+    id: 6,
+    name: 'Nicholas Lain',
+    userName: 'user123',
+  },
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth',
+  },
+  2: {
+    id: 2,
+    name: 'Selma',
+  },
+  3: {
+    id: 3,
+    name: 'Million Dollar Baby',
+  },
+  4: {
+    id: 4,
+    name: 'Forrest Gump',
+  },
+  5: {
+    id: 5,
+    name: 'Get Out',
+  },
+};
+
+class MovieItem extends Component {
+    renderItems(profiles, users) {
+        if (profiles.length <= 0) {
+            return (
+                <div>
+                    <p>None of the current users liked this movie</p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>Liked By:</p>
+                    <ul>
+                    {profiles.map((profile) =>
+                        <li key={profile.userID}>{users[profile.userID].name}</li>
+                    )}
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        const movie = this.props.movie;
+        const profiles =  Object.keys(this.props.profiles).map(key => this.props.profiles[key]);
+        const matchingProfiles = profiles.filter(profile => profile.favoriteMovieID == movie.id);
+        return (
+            <div>
+                <h2>{this.props.movie.name}</h2>
+                {this.renderItems(matchingProfiles, users)}
+            </div>
+        )
+    }
+}
+
+class App extends Component {
+  render() {
+    const movieList = Object.keys(movies).map(key => movies[key]);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">ReactND - Coding Practice</h1>
+            </header>
+            <h2>How Popular is Your Favorite Movie?</h2>
+            {movieList.map((movie => (
+            <MovieItem movie={movie} profiles={profiles} />
+            )))}
+        </div>
+    );
+  }
+}
+
+export default App;
